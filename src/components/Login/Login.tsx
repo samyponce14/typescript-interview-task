672326e37ee18476650f04d3880d42c983af2ada
Samantha Ponce <samy.ponce14@gmail.com>
2021-03-17 17:33:32 +0100

    Add login validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,54 @@
-import {SyntheticEvent, useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {Routes} from '~/constants';
+import { SyntheticEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Routes } from '~/constants';
 import login from '~/services/login';
 import ErrorBlock from '../ErrorBlock';
 
 import './login-style.scss';
 
 const Login = () => {
-  const {push} = useHistory();
+  const { push } = useHistory();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState<string>();
+  const [usernameError, setUsernameError] = useState<string>();
+  const [passwordError, setPasswordError] = useState<string>();
+
+  const requiredValidator = (value?: string) => {
+    return !value;
+  };
 
   const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     setErrorMessage(null);
+    setUsernameError(null);
+    setPasswordError(null);
 
     try {
       await login(username, password);
       push(Routes.PasswordHealth);
     } catch (error) {
       setErrorMessage(error.message);
+    }
+  };
+
+  const validateForm = () => {
+    return requiredValidator(username) && requiredValidator(username);
+  };
+
+  const validateUsername = () => {
+    if (requiredValidator(username)) {
+      setUsernameError('Username is required');
+    } else {
+      setUsernameError(null);
+    }
+  };
+
+  const validatePassword = () => {
+    if (requiredValidator(password)) {
+      setPasswordError('Password is required');
+    } else {
+      setPasswordError(null);
     }
   };
 
@@ -31,21 +59,25 @@ const Login = () => {
           Password Health
         </h1>
         <input
+          className={"input mt-52px " + (usernameError ? 'input-error' : null)}
           value={username}
           onChange={(event) => setUsername(event.target.value)}
+          onBlur={() => validateUsername()}
           placeholder="Username"
           type="text"
-          className="input mt-52px"
         />
+        <ErrorBlock error={usernameError} />
         <input
+          className={"input mt-24px " + (passwordError ? 'input-error' : null)}
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          onBlur={() => validatePassword()}
           placeholder="Password"
           type="password"
-          className="input mt-24px"
         />
-        <ErrorBlock error={errorMessage}/>
-        <button type="submit" className="button mt-24px">
+        <ErrorBlock error={passwordError} />
+        <ErrorBlock error={errorMessage} />
+        <button type="submit" className="button mt-24px" disabled={validateForm()}>
           Login
         </button>
       </form>
